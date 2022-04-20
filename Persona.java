db1c714c589a81d863d2ed56b9c53c13e19aa4c2
masajoPruebas <raul.martos.pruebas@gmail.com>
2022-04-20 13:08:36 +0200

    Mi primera clase Persona.java creada

---
+++ Persona.java
@@ -0,0 +1,11 @@
+public class Persona {
+
+    private final String nombre;
+    private final String apellidos;
+
+    public Persona(String nombre, String apellidos){
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+    }
+
+}
