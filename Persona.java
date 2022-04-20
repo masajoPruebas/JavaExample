678909ccd777e5fa68c5d35dcb699ceba13ae691
masajoPruebas <raul.martos.pruebas@gmail.com>
2022-04-20 13:23:51 +0200

    Clase Persona modificada

--- Persona.java
+++ Persona.java
@@ -3,9 +3,12 @@ public class Persona {
     private final String nombre;
     private final String apellidos;
 
-    public Persona(String nombre, String apellidos){
+    private final String email;
+
+    public Persona(String nombre, String apellidos, String email){
         this.nombre = nombre;
         this.apellidos = apellidos;
+        this.email = email;
     }
 
 }
